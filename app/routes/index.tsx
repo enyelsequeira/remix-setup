3f796358c4255eb31cd33fbebb5f2ad169b5e522
enyelsequeira <enyelsequeira@hotmail.com>
2022-02-26 18:01:45 +0800

    feat: added commitzen to repo
    n

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1 className="text-3xl font-bold underline text-[#f2f]">Hello world!</h1>
+      <p>this is a change</p>
     </div>
   );
 }
